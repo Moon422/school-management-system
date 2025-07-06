c07f8ca62e09633445642f8cf1d93db3e3b9adc5
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2025-07-06 12:47:15 +0600

    added jwt guard and decorators

--- sms-api/src/auth/auth.module.ts
+++ sms-api/src/auth/auth.module.ts
@@ -3,13 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     PassportModule,
-    JwtModule
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRES_IN
+      },
+    })
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule { }
